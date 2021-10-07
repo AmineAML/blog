045d39269418a084fa026824f2c40e345887363d
Amine AML <amin_aml@live.fr>
2021-10-07 23:05:37 +0100

    chore: add author component for name, links and description

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -23,5 +23,20 @@
         </div>
         <div class="divider" />
         <p>ree</p>
+        <div class="card lg:card-side bordered flex">
+            <div class="avatar my-auto">
+                <div class="mb-8 rounded-full w-24 h-24">
+                  <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
+                </div>
+              </div> 
+            <div class="card-body">
+              <h2 class="card-title">Author name</h2>
+              <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+              <!-- <div class="card-actions">
+                <button class="btn btn-primary">Get Started</button> 
+                <button class="btn btn-ghost">More info</button>
+              </div> -->
+            </div>
+          </div>
 	</div>
 </section>
