0672be1ea58f71c34d740fb1f5edd58ab235efdf
Amine AML <amin_aml@live.fr>
2021-10-08 21:06:02 +0100

    chore: add navbar, refactor topics component's style, add sharing links, add about the author, update other styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,22 +27,75 @@
 	<div class="flex flex-col">
 		<h1 class="text-4xl mb-10 font-extrabold mx-auto">Amine Amellouk's Blog</h1>
 		<p class="mx-auto">
-			Software Developer with a passion for the Back-End engineering and the DevOps
+			Software Developer with a passion for the Back-End engineering and the DevOps.
 		</p>
 		<div class="flex space-x-2 mx-auto">
-			<a href="/">LinkedIn</a>
-			<a href="/">GitHub</a>
-			<a href="/">Stack Overflow</a>
+			<a href="https://www.amineamellouk.com">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+					/>
+				</svg>
+			</a>
+			<a href="https://linkedin.com/in/amine-amellouk">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 5 1036 990"
+					class="h-5 w-5"
+					stroke="currentColor"
+				>
+					<path
+						d="M0 120c0-33.334 11.667-60.834 35-82.5C58.333 15.833 88.667 5 126 5c36.667 0 66.333 10.666 89 32 23.333 22 35 50.666 35 86 0 32-11.333 58.666-34 80-23.333 22-54 33-92 33h-1c-36.667 0-66.333-11-89-33S0 153.333 0 120zm13 875V327h222v668H13zm345 0h222V622c0-23.334 2.667-41.334 8-54 9.333-22.667 23.5-41.834 42.5-57.5 19-15.667 42.833-23.5 71.5-23.5 74.667 0 112 50.333 112 151v357h222V612c0-98.667-23.333-173.5-70-224.5S857.667 311 781 311c-86 0-153 37-201 111v2h-1l1-2v-95H358c1.333 21.333 2 87.666 2 199 0 111.333-.667 267.666-2 469z"
+					/>
+				</svg>
+			</a>
+			<a href="mailto:amine.amellouk@outlook.com">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+					/>
+				</svg>
+			</a>
 		</div>
-		<div class="flex space-x-2 mx-auto">
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
-			<button class="badge">topic</button>
+		<div class="flex justify-center flex-wrap">
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
+			<button class="badge mx-2 my-2">topic</button>
 		</div>
 		<div class="divider" />
 		<div class="form-control py-3 mx-auto w-full">
@@ -61,7 +114,9 @@
 		{:then data}
 			{#each data as { id, title, body }}
 				<div class="card bordered shadow-2xl mb-20">
-					<div class="badge badge-ghost">topic</div>
+					<div class="flex space-x-2 flex-wrap">
+						<div class="badge badge-ghost mx-2 my-2">topic</div>
+					</div>
 					<figure class="flex px-10 pt-10">
 						<img
 							src="https://www.artza-technologies.com/img/manageProjectsPage/gestion-projets.png"
@@ -78,7 +133,7 @@
 						<h2 class="card-title">{title}</h2>
 						<p>{body}</p>
 						<div class="card-actions">
-							<a href="/posts/{id}">read more</a>
+							<a href="/posts/{id}" class="text-gray-500">read more</a>
 						</div>
 					</div>
 				</div>
